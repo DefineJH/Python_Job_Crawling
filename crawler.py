80c2d2bdfe42c57172fd7011d8bdf07839c61bf5
JH <wlghks1502@gmailc.om>
2021-06-02 12:36:44 +0900

    Final
    Need more trimming,
    but works fine.

--- crawler.py
+++ crawler.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup as bs
 import urllib.parse
 import webbrowser
 import wordcloud
-import matplotlib
 import requests
 import random
 import time
@@ -12,13 +11,13 @@ recruit_page = list()
 
 keywords = dict()
 
-NGWords = ['서울','시','구','학원']
+NGWords = ['서울','시','구','학원', '지역' , '경기' ,'병역']
 
 
 
-def JobKorea_Search():
+def JobKorea_Search(jobName):
     baseUrl = 'https://www.jobkorea.co.kr/Search/?stext='
-    plusUrl = input('검색어를 입력하세요 : ')
+    plusUrl = jobName
 
 
     url = baseUrl + urllib.parse.quote_plus(plusUrl)
@@ -44,11 +43,10 @@ def JobKorea_SearchPage(html, idx):
     #게임잡 url 나와서 https://www.jobkorea.co.kr/https://www.gamejob.co.kr/List_GI/GIB_Read.asp?GI_No=190409 와같은 일이 일어날 수도 있음.
     for i in company:
         tempUrl = i.attrs['href']
-        if "gamejob" in  tempUrl:
-            tempList = tempUrl.split('http')
-            recruit_page.append('http' + tempList[-1])
+        if tempUrl.count("http") == 2:
+            continue
         else:
-            recruit_page.append(baseRecruit_URL + tempUrl)  
+            recruit_page.append(baseRecruit_URL + tempUrl) 
 
     idx += 1    
     page = bsObject.select_one("a[page-no*=\"" + str(idx) +"\"]")
@@ -70,11 +68,10 @@ def JobKorea_SearchPage_Debug(html, idx):
     #게임잡 url 나와서 https://www.jobkorea.co.kr/https://www.gamejob.co.kr/List_GI/GIB_Read.asp?GI_No=190409 와같은 일이 일어날 수도 있음.
     for i in company:
         tempUrl = i.attrs['href']
-        if "gamejob" in  tempUrl:
-            tempList = tempUrl.split('http')
-            recruit_page.append('http' + tempList[-1])
+        if tempUrl.count("http") == 2:
+            continue
         else:
-            recruit_page.append(baseRecruit_URL + tempUrl)  
+            recruit_page.append(baseRecruit_URL + tempUrl) 
     return
 
 
@@ -110,8 +107,9 @@ def JobKorea_KeywordListing():
 
 
 
-def StartCrawling():
-    JobKorea_Search()
+def StartCrawling(jobName):
+    
+    JobKorea_Search(jobName)
     JobKorea_KeywordListing()
 
     for elem in recruit_page:
@@ -120,10 +118,80 @@ def StartCrawling():
         print(key, ":", value)
 
     wordCloud = wordcloud.WordCloud(font_path='font_korean.ttf',background_color='white',width=1000,height=1000).generate_from_frequencies(keywords)
+    wordCloud.to_file('img.png')
 
-    fig = matplotlib.pyplot.figure()
-    matplotlib.pyplot.imshow(wordCloud, interpolation='bilinear')
-    matplotlib.pyplot.axis('off')
-    matplotlib.pyplot.show()
-    matplotlib.pyplot.savefig('img.jpg')
+
+def ConstructInfoSite(keyword):
+    html_text_start = """
+     <!DOCTYPE html>
+     <html>
+     <head>
+     <title>설명 페이지</title>
+     </head>
+     <body>
+ """    
+    html_text_end = """
+     </body>
+     </html>
+ """
+    html_middle = ""
+    baseUrl = 'https://www.google.com/search?q='
+    url = baseUrl +  urllib.parse.quote_plus(keyword)
+    session = requests.Session()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    }
+    html = session.get(url, headers=headers).content
+    bsObject = bs(html, "html.parser")
+    v = bsObject.select('.yuRUbf')
+    wiki = ""
+    html_middle += "<div> <h1> 구글검색 </h1> <br>"
+    for i in v:
+        urls = i.a.attrs['href']
+        if "namu" in i.a.attrs['href']:
+            wiki += ("<button onclick=\"location.href=\'" + urls + "\'\">나무위키 " + keyword + "검색 </button>")
+        elif "wikipedia" in i.a.attrs['href']:
+            wiki += ("<button onclick=\"location.href=\'" + urls + "\'\">위키피디아 " + keyword + "검색 </button>")
+        else:
+            html_middle +="<a href=\""+urls+"\"><h3>"+i.select_one('.LC20lb.DKV0Md').text+"</h3></a>"
+        print(i.select_one('.LC20lb.DKV0Md').text) # 제목
+        print(i.a.attrs['href'])       # 링크
+
+    html_middle += "<div> <h1> 위키 사이트 </h1> <br>"+wiki
+    html_middle += "</div>"
+
+    html_middle += "<div> <h1> 추천서적 </h1> <br>"
+    #
+    books = list()
+    baseUrl = 'http://www.yes24.com/searchcorner/Search?keywordAd=&keyword=&domain=ALL&qdomain=%C0%FC%C3%BC&Wcode=001_005&query='
+
+    enc = urllib.parse.quote(keyword)
+    url = baseUrl + urllib.parse.quote_plus(keyword,encoding="euc-kr")
+    print(enc)
+    print(url)
+
+
+    session = requests.Session()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    }
+    html = session.get(url, headers=headers).content
+    bsObject = bs(html, "html.parser")
+    v = bsObject.select('a.img_bdr')
+    for elem in v:
+        tempURL = "https://www.yes24.com" +  elem['href']
+        imgTag = elem.find("img")
+        tempImg = imgTag["src"]
+        tempName = imgTag["alt"]
+        books.append([tempURL,tempImg,tempName])
+
+
+    for b in books:
+        html_middle += "<a href=\""+b[0]+"\" >"+"<img width=100 src=\""+b[1]+"\"></a><br><p>"+b[2]+"</p>"
+        
+    html_middle += "</div>"
+    f = open("final_page.html",'w')
+    f.write(html_text_start + html_middle + html_text_end)
+
+    webbrowser.open("final_page.html")
 
--- main.py
+++ main.py
@@ -1,11 +1,77 @@
+import threading
 import tkinter as tk
 import crawler
+from PIL import ImageTk, Image
+import webbrowser
+myThread = threading.Thread()
+
+def makeWaitUI():
+    wnd = tk.Tk()
+    lbl = tk.Label(wnd,text="직업이름")
+    lbl.grid(row=0, column=0)
+    txt = tk.Entry(wnd)
+    txt.grid(row=0, column=1)
+    def OnClick():
+        jobName = txt.get()
+        wnd.destroy()
+        makeLoadingUI(jobName)
+    btn = tk.Button(wnd, text="OK", command=OnClick)
+    btn.grid(row=1, column=1)
+    lbl2 = tk.Label(wnd, text="작업시간에는 대략 3~4분이 소요됩니다.\n 작업종료 시 새 창으로 알려드립니다.")
+    lbl2.grid(row=2, column=0)
+    wnd.mainloop()
+    
+def makeLoadingUI(jobName):
+    global myThread
+    myThread = threading.Thread(target=crawler.StartCrawling,args=(jobName,))
+    myThread.start()
+    myThread.join()
+    makeResultUI()
 
 
-crawler.StartCrawling()
+def ConstructInfoSite(keyword):
+    crawler.ConstructInfoSite(keyword)
 
-wnd = tk.Tk()
+def makeResultUI():
+    wnd = tk.Tk()
+
+    lbl2 = tk.Label(wnd, text="아래 키워드들 중 더 자세한 정보를 알고싶은 키워드를 누르고 버튼 클릭 시 웹페이지로 연결됩니다. ")
+    lbl2.pack()
+    
+    img = Image.open("img.png")
+    img = img.resize((500,500),Image.ANTIALIAS)
+
+    test = ImageTk.PhotoImage(img)
+    lbl = tk.Label(wnd, width=500, height=500, image=test)      
+    lbl.pack()      
+    
+    keywordSorted = sorted(crawler.keywords.items(),key=(lambda x :x[1]),reverse=True)
+
+    frame = tk.Frame(wnd)
+    scrollbar = tk.Scrollbar(wnd)
+    scrollbar.pack(side="right",fill="y")
+    listbox = tk.Listbox(frame,yscrollcommand=scrollbar.set)
+    
+    idx = 0
+    for item in keywordSorted:
+        listbox.insert(idx,str(item[0]) + " mentioned " + str(item[1]) + " times.")
+        idx = idx + 1
 
 
-wnd.mainloop()
+    listbox.pack(side="left",fill=tk.BOTH,expand=True)
 
+    scrollbar["command"]=listbox.yview
+    frame.pack(fill=tk.BOTH,expand=True)
+
+    def OnClick():
+        tempTuple = listbox.curselection()
+        tempStr = listbox.get(tempTuple[0])
+        strings = tempStr.split("mentioned")
+        ConstructInfoSite(strings[0])
+
+
+    btn = tk.Button(wnd, text="GOTO", command=OnClick)
+    btn.pack()
+    wnd.mainloop()
+    
+makeWaitUI()
